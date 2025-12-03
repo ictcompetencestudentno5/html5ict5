d8fb553b97caca964af712a6d22211e8fa99c4a6
ictcompetencestudentno5@gmail.com <IctCompetenceStudentNo5@gmail.com>
2025-12-03 09:01:57 +0800

    123

--- test/test1.js
+++
@@ -1,9 +0,0 @@
-function add() {
-    let sum = 1 + 1;
-    let binarysum = sum.toString(2);
-        document.getElementById("wait").innerHTML = "wait";
-    setTimeout(function() {
-        document.getElementById("paragraph").innerText = "1 + 1 = " + binarysum;
-        document.getElementById("wait").innerHTML = "";
-    }, 3000)
-}
